3b2a4ede22f16fcf78e12e389d1bf89a6b2cf62d
Abhilash Mundepi <160834131+AbhilashMundepi@users.noreply.github.com>
2026-01-08 07:19:02 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -77,7 +77,7 @@ app.post("/recommend", async (req, res) => {
 });
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT =  process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
